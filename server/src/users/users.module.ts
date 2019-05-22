47160164e861e17e2933e2a9111d34d11a697cb3
vaiulian <vasileaiulian@gmail.com>
2019-05-22 22:21:14 +0300

    Add Passport default strategy as JWT

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
   providers: [UsersService],
   controllers: [UsersController],
