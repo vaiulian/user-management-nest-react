8d7f9d015bd882441307ee00e04b10d9ba6c68e4
vaiulian <vasileaiulian@gmail.com>
2019-05-22 22:25:07 +0300

    Refactor Users.service: - add throw NotFoundException - fix updateUser for Entity User - fix updateUser to hashPasswords - add custom HttpException when adding new User with existing userName

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpException, HttpStatus } from '@nestjs/common';
 import { Repository, UpdateResult, DeleteResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import * as passwordManager from '../utility/passwordManager';
 
 @Injectable()
 export class UsersService {
@@ -11,23 +12,53 @@ export class UsersService {
     ) { }
 
     async findAll(): Promise<User[]> {
-        return await this.userRepository.find();
+        const userData: User[] = await this.userRepository.find();
+        return this.throwErrorOrReturn(userData);
     }
 
     async findOne(id: number): Promise<User> {
-        return await this.userRepository.findOne({id});
+        const userData: User = await this.userRepository.findOne({id});
+        return this.throwErrorOrReturn(userData);
+    }
+
+    async findByUserName(userName: string): Promise<User> {
+        const userData: User = await this.userRepository.findOne({
+            where: { userName },
+        });
+        return this.throwErrorOrReturn(userData);
     }
 
     async addUser(user: User): Promise<User> {
         const entity = Object.assign(new User(), user);
-        return await this.userRepository.save(entity);
+        let userData: User;
+        try {
+            userData = await this.userRepository.save(entity);
+        } catch (e) {
+            throw new HttpException({
+                status: HttpStatus.FORBIDDEN,
+                error: e.message,
+              }, HttpStatus.FORBIDDEN);
+        }
+        return this.throwErrorOrReturn(userData);
     }
 
     async updateUser(user: User): Promise<UpdateResult> {
-        return await this.userRepository.update(user.id, user);
+        const entity: User = Object.assign(new User(), user);
+        if (entity.password && entity.password !== '') {
+            entity.password = passwordManager.generateHash(entity.password);
+        }
+
+        return await this.userRepository.update(entity.id, entity);
     }
 
     async deleteUser(id: number): Promise<DeleteResult> {
         return await this.userRepository.delete(id);
+    }
+
+    throwErrorOrReturn = (data: any) => {
+        if (!data) {
+            throw new NotFoundException();
+        }
+        return data;
     }
 }
