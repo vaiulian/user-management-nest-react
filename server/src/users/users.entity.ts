f26e7a8184accb07b694da8d087a170bd93b8b66
vaiulian <vasileaiulian@gmail.com>
2019-05-22 22:27:20 +0300

    Add Unique column userName and exclude password from  User object on API

--- server/src/users/users.entity.ts
+++ server/src/users/users.entity.ts
@@ -1,11 +1,13 @@
-import { Entity, Column, BeforeInsert, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, BeforeInsert, PrimaryGeneratedColumn, Unique } from 'typeorm';
 import * as passwordManager from '../utility/passwordManager';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Unique(['userName'])
     @Column()
     userName: string;
 
@@ -19,6 +21,7 @@ export class User {
     hashPassword() {
         this.password = passwordManager.generateHash(this.password);
     }
+    @Exclude()
     @Column()
     password: string;
 }
