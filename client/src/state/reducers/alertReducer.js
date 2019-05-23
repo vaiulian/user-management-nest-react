4a3a92f1eb6c42312ac6c804350e341599eae97d
vaiulian <vasileaiulian@gmail.com>
2019-05-23 09:34:50 +0300

    Add alert reducer and actions

---
+++ client/src/state/reducers/alertReducer.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/prefer-default-export */
+import { alertConstants } from '../constants/alert.constants';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case alertConstants.SUCCESS:
+      return {
+        type: 'alert-success',
+        message: action.message
+      };
+    case alertConstants.ERROR:
+      return {
+        type: 'alert-danger',
+        message: action.message
+      };
+    case alertConstants.CLEAR:
+      return {};
+    default:
+      return state
+  }
+}
