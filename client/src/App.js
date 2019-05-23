900263ce2dffc1428b075126b705d573b037f7dc
vaiulian <vasileaiulian@gmail.com>
2019-05-23 11:52:35 +0300

    Update App with redux and router history

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route, Router, Switch } from 'react-router-dom'
+import { history } from './utility/history';
 import Users from './pages/users'
 import User from './pages/user'
 import Home from './pages/home'
 import Login from './pages/login'
 import Register from './pages/register'
 import FourOFours from './pages/404'
+
+
 import './App.css';
 
 import { ProtectComponent } from './components/ProtectComponent';
@@ -21,24 +24,39 @@ class App extends Component {
   }
 
   render() {
+    const { alert } = this.props;
     return (
       <div className="App">
-        <Router>
-          <div>
-            <Switch>
-              <Route path="/login" component={Login} />
-              <Route path="/register" component={Register} />
-              <ProtectComponent exact path="/" component={Home} />
-              <ProtectComponent exact path="/users" component={Users} />
-              <ProtectComponent path="/users/:id" component={User} />
-              <Route component={FourOFours} />
-            </Switch>
+        <div className="container">
+          <div className="col-sm-12">
+            {alert.message &&
+              <div className={`alert ${alert.type}`}>{alert.message}</div>
+            }
+            <Router history={history} forceRefresh>
+              <div>
+                <Switch>
+                  <Route path="/login" component={Login} />
+                  <Route path="/register" component={Register} />
+                  <ProtectComponent exact path="/" component={Home} />
+                  <ProtectComponent exact path="/users" component={Users} />
+                  <ProtectComponent path="/users/:id" component={User} />
+                  <Route component={FourOFours} />
+                </Switch>
+              </div>
+            </Router>
           </div>
-        </Router>
+        </div>
       </div>
 );
   }
 
 }
 
-export default connect()(App);
+function mapStateToProps(state) {
+  const { alert } = state;
+  return {
+      alert
+  };
+}
+
+export default connect(mapStateToProps)(App);
